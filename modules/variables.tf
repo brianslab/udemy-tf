57b5d196c0a0354184c71eb468351d74e6ea47ed
brianslab <bdkuhn602@gmail.com>
2023-03-27 04:29:08 -0700

    dev and prod port validation

--- modules/variables.tf
+++ modules/variables.tf
@@ -16,10 +16,14 @@ variable "image" {
 variable "ext_port" {
   type = map(list(number))
 
-  # validation {
-  #   condition     = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
-  #   error_message = "The external port must be between 0 and 65535."
-  # }
+  validation {
+    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The external port must be between 0 and 65535."
+  }
+  validation {
+    condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The external port must be between 0 and 65535."
+  }
 }
 
 variable "int_port" {
