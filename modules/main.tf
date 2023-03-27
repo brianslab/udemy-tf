9d68ac3a4ed9be382acd9dd6e87ab234cb347860
brianslab <bdkuhn602@gmail.com>
2023-03-27 00:51:00 -0700

    random example

--- modules/main.tf
+++ modules/main.tf
@@ -13,21 +13,50 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  length  = 4
+  special = false
+}
+
+resource "random_string" "random2" {
+  length  = 4
+  special = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    # external = 1880
   }
 }
 
 output "IP-Address" {
   value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
-  description = "The IP addressand external port of the container"
+  description = "The IP addressand external port of the first container"
 }
 
 output "container-name" {
   value       = docker_container.nodered_container.name
-  description = "The name of the container"
+  description = "The name of the first container"
+}
+
+output "IP-Address-2" {
+  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
+  description = "The IP addressand external port of the container"
+}
+
+output "container-name-2" {
+  value       = docker_container.nodered_container2.name
+  description = "The name of the second container"
 }
