328461fd1999f0b2881106d6c9be0ce761b93f50
brianslab <bdkuhn602@gmail.com>
2023-03-28 03:15:53 -0700

    use map key instead of lookup

--- modules/main.tf
+++ modules/main.tf
@@ -16,7 +16,7 @@ resource "null_resource" "dockervol" {
 }
 
 resource "docker_image" "nodered_image" {
-  name = lookup(var.image, terraform.workspace)
+  name = var.image[terraform.workspace]
 }
 
 resource "random_string" "random" {
@@ -31,7 +31,7 @@ resource "docker_container" "nodered_container" {
   image = docker_image.nodered_image.image_id
   ports {
     internal = var.int_port
-    external = lookup(var.ext_port, terraform.workspace)[count.index]
+    external = var.ext_port[terraform.workspace][count.index]
   }
   volumes {
     container_path = "/data"
