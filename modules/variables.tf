a4d1dd0dfa1562f6c1d7dbc94317236d50265a11
brianslab <bdkuhn602@gmail.com>
2023-03-28 03:12:46 -0700

    use workspaces

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,9 +1,3 @@
-variable "env" {
-  type        = string
-  description = "Env to deploy to"
-  default     = "dev"
-}
-
 variable "image" {
   type        = map(string)
   description = "image for container"
@@ -37,5 +31,5 @@ variable "int_port" {
 }
 
 locals {
-  container_count = length(lookup(var.ext_port, var.env))
+  container_count = length(lookup(var.ext_port, terraform.workspace))
 }
