0b6e1699b319c4cffc07b67980324b089676c97f
brianslab <bdkuhn602@gmail.com>
2023-03-28 04:00:35 -0700

    create container module

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,9 +1,9 @@
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The names of the containers"
-}
+# output "container-name" {
+#   value       = docker_container.nodered_container[*].name
+#   description = "The names of the containers"
+# }
 
-output "IP-Address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP addresses and external port of the containers"
-}
+# output "IP-Address" {
+#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+#   description = "The IP addresses and external port of the containers"
+# }
