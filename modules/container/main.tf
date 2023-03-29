be85ef579cf2f11701448d59b1bd89d0f2246a3f
brianslab <bdkuhn602@gmail.com>
2023-03-29 04:38:52 -0700

    use self in local provisioners

--- modules/container/main.tf
+++ modules/container/main.tf
@@ -24,4 +24,14 @@ resource "docker_volume" "container_volume" {
   lifecycle {
     prevent_destroy = false
   }
+  provisioner "local-exec" {
+    when       = destroy
+    command    = "mkdir ~/Backups/docker/"
+    on_failure = continue
+  }
+  provisioner "local-exec" {
+    when       = destroy
+    command    = "sudo tar -czvf ~/Backups/docker/${self.name}.tar.gz ${self.mountpoint}"
+    on_failure = fail
+  }
 }
